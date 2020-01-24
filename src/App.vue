60164ef95f6398b3347143083cd04cd39371e289
emedee <emediongini@gmail.com>
2020-01-24 11:54:29 -1200

    commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <app-form/>
+  </div>
+</template>
+
+<script>
+import Forms from '@/components/Forms.vue'
+export default {
+  components: {
+    'app-form': Forms
+  }
+}
+</script>
---
+++ src/components/Forms.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-8">
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">{{ headerOne }}</th>
+                            <th scope="col">{{ headerTwo }}</th>
+                            <th scope="col">{{ headerThree }}</th>
+                            <th scope="col">{{ headerFour }}</th>
+                            <th scope="col">{{ headerFive }}</th>
+                            <th scope="col">{{ headerSix }}</th>
+                            <th scope="col">{{ headerSeven }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for='(person,i) in persons'>
+                            <th scope="row">{{ parseInt(i) + 1 }}</th>
+                            <td>{{ person.name }}</td>
+                            <td>{{person.email}}</td>
+                            <td>{{person.role}}</td>
+                            <td>{{person.city}}</td>
+                            <td>{{person.score}}</td>
+                            <td>
+                                <button class="btn btn-danger">Delete</button>
+                                <button class="btn btn-primary">Edit</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow p-4">
+                    <h2 class="mb-4">{{ message }}</h2>
+                    <form>
+                        <div class="form-group">
+                            <label>{{ message2 }}</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>{{ message3 }}</label>
+                            <input type="Email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>{{ message4 }}</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>{{ message5 }}</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>{{ message6 }}</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary ml-3">{{ message7 }}</button>
+                            <button type="button" class="btn btn-secondary">{{ message8 }}</button>
+                            <button type="reset" class="btn btn-danger">{{ message9 }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            headerOne: 'ID',
+            headerTwo: 'NAME',
+            headerThree: 'EMAIL',
+            headerFour: 'ROLE',
+            headerFive: 'CITY',
+            headerSix: 'SCORE',
+            message1: 'Please add new users',
+            message2: 'Name',
+            message3: 'Email',
+            message4: 'Role',
+            message5: 'City',
+            message6: 'Score',
+            message7: 'Submit',
+            message8: 'Update',
+            message9: 'Reset',
+            user: {
+                name: "",
+                role: "",
+            },
+            persons: [
+                {
+                    name:  'wisdom', 
+                    email: 'wisdom425@gmail.com',
+                    role:  'Back-end Developer',
+                    city:  'Uyo',
+                    score: 45
+                },
+                {
+                    name:  'Ruth', 
+                    email: 'ruth321@gmail.com',
+                    role:  'Android Developer',
+                    city:  'Calabar',
+                    score: 52
+                },
+                {
+                    name:  'Steve', 
+                    email: 'stevey25@gmail.com',
+                    role:  'AI Developer',
+                    city:  'Abuja',
+                    score: 60
+                },
+                {
+                    name:  'Martha', 
+                    email: 'martha005@gmail.com',
+                    role:  'PHP Developer',
+                    city:  'PortHarcourt',
+                    score: 55
+                },
+                {
+                    name:  'Samuel', 
+                    email: 'samuel523@gmail.com',
+                    role:  'Javascript Developer',
+                    city:  'Uyo',
+                    score: 70
+                },
+            ],
+            status: true
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
